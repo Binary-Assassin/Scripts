8c5ddb946b4cdbb61436145d283009f01e94da53
Mehdi Badami <98187755+Binary-Assassin@users.noreply.github.com>
2025-11-21 16:01:39 +0500

    Create burp_launcher.bat to start Burp Suite
    Add batch file to launch Burp Suite with specific path.

---
+++ burp_lancher.bat
@@ -0,0 +1,6 @@
+@echo off
+# cd /toward/where/your/burp/pro/existed/
+cd /d "D:\zstuff\Burp Suite Professional Edition v2025.6.1 x64 Full Activated + Extensions - WwW.Dr-FarFar.CoM\Burp Suite Professional Edition"
+# run cmd
+java -jar Dr-FarFar.jar
+# save it to desktop
